f7f4f14f808d70fe7f0b324ebe027e189ad7bc32
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-04-08 10:25:18 +1000

    Code to inspect code coverage.

---
+++ CoverageTests/BusinessLogicCoverageTest.cs
@@ -0,0 +1,16 @@
+﻿using CoveredLibrary;
+using NUnit.Framework;
+
+namespace CoverageTests
+{
+    [TestFixture]
+    public class BusinessLogicCoverageTest
+    {
+        [Test]
+        public void ExecuteCoveredCode()
+        {
+            var bl = new BusinessLogicClass();
+            bl.Execute();
+        }
+    }
+}
---
+++ CoveredLibrary/BusinessLogicClass.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace CoveredLibrary
+{
+    public class BusinessLogicClass
+    {
+        public void Execute()
+        {
+            Sequence();
+            IfExecuted();
+            IfUnexecuted();
+            IfTrueBranchExecuted();
+            IfFalseBranchExecuted();
+            ForExecutedPartially();
+            ForExecutedTotally();
+            CaseBranchExecuted();
+        }
+
+        private void Sequence()
+        {
+            Console.WriteLine("Executed");
+        }
+
+        private void IfExecuted()
+        {
+            var i = 1;
+            if (i == 1)
+            {
+                Console.WriteLine("Executed");
+            }
+        }
+
+        private void IfUnexecuted()
+        {
+            var i = 0;
+            if (i == 1)
+            {
+                Console.WriteLine("Non-executed");
+            }
+        }
+
+        private void IfTrueBranchExecuted()
+        {
+            var i = 1;
+            if (i == 1)
+            {
+                Console.WriteLine("Executed");
+            }
+            else
+            {
+                Console.WriteLine("Non-executed");
+            }
+        }
+
+        private void IfFalseBranchExecuted()
+        {
+            var i = 0;
+            if (i == 1)
+            {
+                Console.WriteLine("Executed");
+            }
+            else
+            {
+                Console.WriteLine("Non-executed");
+            }
+        }
+
+        private void ForExecutedPartially()
+        {
+            for (var i = 0; i < 2; ++i)
+            {
+                Console.WriteLine("Executed");
+                return;
+            }
+        }
+
+        private void ForExecutedTotally()
+        {
+            for (var i = 0; i < 2; ++i)
+            {
+                Console.WriteLine("Executed");
+            }
+        }
+
+        private void CaseBranchExecuted()
+        {
+            var i = 1;
+            switch (i % 3)
+            {
+                case 0:
+                    Console.WriteLine("Non-executed");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Executed");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Non-executed");
+                    break;
+            }
+        }
+    }
+}
